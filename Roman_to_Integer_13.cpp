516bbc41d95d630be45f1fa2959f6a07553fbd61
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-07-23 23:19:57 -0500

    Update Roman_to_Integer_13.cpp

--- Roman_to_Integer_13.cpp
+++ Roman_to_Integer_13.cpp
@@ -3,8 +3,14 @@ public:
     int romanToInt(string s) {
         int exception = 0, Is = 0, Vs = 0, Xs = 0, Ls = 0, Cs = 0, Ds = 0, Ms = 0;
         for (int i = 0; i < s.length(); i++) {
-            s[i]
+            if (s[i] == 'I') {Is += 1;}
+            if (s[i] == 'V') {Vs += 1;}
+            if (s[i] == 'X') {Xs += 1;}
+            if (s[i] == 'L') {Ls += 1;}
+            if (s[i] == 'C') {Cs += 1;}
+            if (s[i] == 'D') {Ds += 1;}
+            if (s[i] == 'M') {Ms += 1;}
         }
-        return exception + Is * 1 + Vs * 5 + Xs * 10 + Ls * 50 + Cs * 100 + Ds * 500 + Ms * 1000
+        return exception + Is * 1 + Vs * 5 + Xs * 10 + Ls * 50 + Cs * 100 + Ds * 500 + Ms * 1000;
     }
 };
