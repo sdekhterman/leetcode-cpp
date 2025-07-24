5ca10e1b27b2b9fe67f37c1f72bacbbe3c2e18b0
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-07-23 23:14:48 -0500

    Create Roman_to_Integer_13.cpp

---
+++ Roman_to_Integer_13.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int exception = 0, Is = 0, Vs = 0, Xs = 0, Ls = 0, Cs = 0, Ds = 0, Ms = 0;
+        for (int i = 0; i < s.length(); i++) {
+            s[i]
+        }
+        return exception + Is * 1 + Vs * 5 + Xs * 10 + Ls * 50 + Cs * 100 + Ds * 500 + Ms * 1000
+    }
+};
