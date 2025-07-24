60e89fe7eba2710ddaebc3f634f362b9b53d12f8
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-07-24 09:37:54 -0500

    Update Roman_to_Integer_13.cpp
    I added comments and reduced the number of variables being summed.

--- Roman_to_Integer_13.cpp
+++ Roman_to_Integer_13.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int exception = 0, Is = 0, Vs = 0, Xs = 0, Ls = 0, Cs = 0, Ds = 0, Ms = 0;
+        // value of each character and a running total 
+        const int iVal = 1, vVal = 5, xVal = 10, lVal = 50, cVal = 100, dVal = 500, mVal = 1000;
+        int total = 0;
+
+        // need to start from leftmost char to catch subtraction exceptions
         for (int i = 0; i < s.length(); i++) {
+            // handle the subtraction exceptions (example 4 = IV, I = 1 V = 5)
             if(i<(s.length()-1)){
                 if((s[i] == 'I')&&((s[i+1] == 'V') || (s[i+1] == 'X'))){
-                    Is -= 1;
-                    i++;
+                    total -= iVal; i++;
                 }
                 if((s[i] == 'X')&&((s[i+1] == 'L') || (s[i+1] == 'C'))){
-                    Xs -= 1;
-                    i++;
+                    total -= xVal; i++;
                 }
                 if((s[i] == 'C')&&((s[i+1] == 'D') || (s[i+1] == 'M'))){
-                    Cs -= 1;
-                    i++;
+                    total -= cVal; i++;
                 }
             }
-            
-            if (s[i] == 'I') {Is += 1;}
-            if (s[i] == 'V') {Vs += 1;}
-            if (s[i] == 'X') {Xs += 1;}
-            if (s[i] == 'L') {Ls += 1;}
-            if (s[i] == 'C') {Cs += 1;}
-            if (s[i] == 'D') {Ds += 1;}
-            if (s[i] == 'M') {Ms += 1;}
+
+            // add char to count normally otherwise
+            if (s[i] == 'I') {total += iVal;}
+            if (s[i] == 'V') {total += vVal;}
+            if (s[i] == 'X') {total += xVal;}
+            if (s[i] == 'L') {total += lVal;}
+            if (s[i] == 'C') {total += cVal;}
+            if (s[i] == 'D') {total += dVal;}
+            if (s[i] == 'M') {total += mVal;}
         }
-        return Is * 1 + Vs * 5 + Xs * 10 + Ls * 50 + Cs * 100 + Ds * 500 + Ms * 1000;
+        return total;
     }
 };
