b1b452eb9ed1b0df0539fef9895435bb133f6b90
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-04 23:29:25 -0500

    Update 21_Merge_Two_Sorted_Lists.cpp

--- 21_Merge_Two_Sorted_Lists.cpp
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -12,11 +12,12 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1->val >= list2->val){
-            ListNode node(list1->val, mergeTwoLists(list1, list2));
+            ListNode newList1(list1->next->val, list1->next->next);
+            ListNode node(list1->val, mergeTwoLists(&newList1, list2));
             return &node;
         } else{
-            list2->next
-            ListNode node(list2->val, mergeTwoLists(list1, list2));
+            ListNode newList2(list2->next->val, list2->next->next);
+            ListNode node(list2->val, mergeTwoLists(list1, &newList2));
             return &node;
         }
         // try reducing the size of list1 and list2 as you add a value 
