f559d8cef9c619d67683d9a6fc71cebe9d687f0a
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-04 22:16:17 -0500

    Update and rename Valid_Parentheses_20 to Valid_Parentheses_20.cpp

--- Valid_Parentheses_20.cpp
+++ Valid_Parentheses_20.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+        // go through the string
         for(int i = 0; i < s.length(); i++){
+            // add newest open bracket to stack
             if((s[i] == '(') || (s[i] == '{') || (s[i] == '[')){st.push(s[i]); continue;}
+            // cover edge case where no open brackets
             if(st.size() == 0) {return false;}
+            //removed matching open bracket from stack
             if((st.top() == '(') && (s[i] == ')')){st.pop();}
             else if((st.top() == '{') && (s[i] == '}')){st.pop();}
             else if((st.top() == '[') && (s[i] == ']')){st.pop();}
+            // cover edge case where there are mismatching open and closing bracket.
             else{return false;}
         }
         return (st.size() == 0);
