b2acfed6cbbc15229bba434152d2d8582d83a485
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-05 23:17:43 -0500

    Update 21_Merge_Two_Sorted_Lists.cpp

--- 21_Merge_Two_Sorted_Lists.cpp
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -11,15 +11,44 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1->val >= list2->val){
-            ListNode newList1(list1->next->val, list1->next->next);
-            ListNode node(list1->val, mergeTwoLists(&newList1, list2));
-            return &node;
-        } else{
-            ListNode newList2(list2->next->val, list2->next->next);
-            ListNode node(list2->val, mergeTwoLists(list1, &newList2));
-            return &node;
+        if((list1 == nullptr) && (list2 == nullptr))       {return nullptr;}
+        else if ((list1 != nullptr) && (list2 == nullptr)) {return mergeList1(list1, list2);}
+        else if ((list1 == nullptr) && (list2 != nullptr)) {return mergeList2(list1, list2);} 
+        else if (list1->val < list2->val)                  {return mergeList1(list1, list2);} 
+        else                                               {return mergeList2(list1, list2);}
+
+        return nullptr;
+    }
+private:
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    ListNode* mergeList1(ListNode* list1, ListNode* list2) {
+        if (!list1->next) {
+            ListNode* node = new ListNode(list1->val, mergeTwoLists(nullptr, list2));
+            return node;
+        } else if (!list1->next->next) {
+            ListNode* tail = new ListNode(list1->next->val);
+            ListNode* node = new ListNode(list1->val, mergeTwoLists(tail, list2));
+            return node;
+        } else {
+            ListNode* tail = new ListNode(list1->next->val, list1->next->next);
+            ListNode* node = new ListNode(list1->val, mergeTwoLists(tail, list2));
+            return node;
         }
-        // try reducing the size of list1 and list2 as you add a value 
+    }
+
+    ListNode* mergeList2(ListNode* list1, ListNode* list2) {
+        if (!list2->next) {
+            ListNode* node = new ListNode(list2->val, mergeTwoLists(list1, nullptr));
+            return node;
+        } else if (!list2->next) {
+            ListNode* tail = new ListNode(list2->next->val);
+            ListNode* node = new ListNode(list2->val, mergeTwoLists(list1, tail));
+            return node;
+        } else {
+            ListNode* tail = new ListNode(list2->next->val, list2->next->next);
+            ListNode* node = new ListNode(list2->val, mergeTwoLists(list1, tail));
+            return node;
+        }
     }
 };
