1fdeaffc28f16274d0d29125f803cf2269fa88f1
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-17 18:49:38 -0500

    Update 27_Remove_Element.cpp

--- 27_Remove_Element.cpp
+++ 27_Remove_Element.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // track number of elements vs pointer at the back of the list
+        int backPointer  = nums.size();
 
+        // go up to number of original number
+        for(int i = 0; i < backPointer; i++) {
+            // check  when need to swap values
+            if(nums[i] == val){
+                // used to find the next original value in the in the back of the list to swap 
+                // with no overlap  with front pointer i
+                do{
+                    backPointer--;
+                    if(backPointer == 0 || backPointer ==i) {
+                        break;
+                    }
+                }
+                while(nums[backPointer] == val);
+                nums[i] = nums[backPointer];
+
+            }
+            
+        }
+        return backPointer;
+    }
+};
