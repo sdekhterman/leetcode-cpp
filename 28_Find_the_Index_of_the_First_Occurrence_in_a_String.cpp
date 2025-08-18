c9fd1861d367d45e9c6e19ba7b558bb155af8231
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-17 20:38:39 -0500

    Update 28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp

--- 28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        // two pointer (index) algoithm
         for(int i = 0; i < haystack.size(); i++){
             if(haystack[i] == needle[0]){
                 for(int j = 0; j < needle.size(); j++){
