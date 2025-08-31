3fcc417f85c14bde803fee33293716f9bc77c5fa
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-08-31 17:38:19 -0500

    Update 58_Length of Last Word.cpp

--- 58_Length of Last Word.cpp
+++ 58_Length of Last Word.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        // edge case: empty 
+        if(s.empty()) {return 0;}
+        
+        while(s.back() == ' ') {
+            s.pop_back();
+        }
+        
+        // edge case: just spaces 
+        if(s.empty()) {return 0;}
+        
+        int length = 0;
+        while(s.back() != ' '){
+            s.pop_back();
+            length++;
 
+            // edge case: no spaces before one word 
+            if(s.empty()) {break;}
+        }
+        return length;
+
+    }
+};
