51ebe82cc20d6c034e362159bf0b9f09a44bcc08
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-09-01 11:52:20 -0500

    Update 66_Plus_One.cpp

--- 66_Plus_One.cpp
+++ 66_Plus_One.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; i--) {
+            if(digits[i] != 9) {
+                digits[i] += 1;
+                break;
+            } else{
+                // digit rollovers
+                digits[i] = 0;
+                if(i==0){digits.insert(digits.begin(),1);}
+            }
+        }
+        return digits;
+    }
+};
