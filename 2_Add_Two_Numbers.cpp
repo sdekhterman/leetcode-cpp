fb8d2b42f3f0ab42d32d7442ede8f5705317628b
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2025-12-07 01:51:23 -0600

    Refactor addTwoNumbers to handle overflow and nulls

--- 2_Add_Two_Numbers.cpp
+++ 2_Add_Two_Numbers.cpp
@@ -8,9 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ #include <iostream>
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        bool l1NextNull = (l1->next == nullptr);
+        bool l2NextNull = (l2->next == nullptr);
+        bool isOverflow = false;
+        int valueSum    = l1->val + l2->val;
+        if(valueSum > 9) {
+            valueSum = valueSum%10;
+            isOverflow = true;
+        } 
+
+        ListNode* list = new ListNode(valueSum);
         
+        if(l1NextNull && !l2NextNull){    
+            if(isOverflow) {l2->next->val = (l2->next->val + 1);}
+            ListNode* list1 = new ListNode();
+            list->next = addTwoNumbers(list1, l2->next);
+        } 
+        else if(!l1NextNull && l2NextNull){
+            if(isOverflow) {l1->next->val = (l1->next->val + 1);}
+            ListNode* list2 = new ListNode();
+            list->next = addTwoNumbers(l1->next, list2);
+        } 
+        else if(!l1NextNull && !l2NextNull){
+            if(isOverflow) {l2->next->val = (l2->next->val + 1);}
+            list->next = addTwoNumbers(l1->next, l2->next);
+        } 
+        else if(isOverflow){
+            ListNode* list1 = new ListNode(1);
+            ListNode* list2 = new ListNode();
+            list->next = addTwoNumbers(list1, list2);
+        }
+        return list;
     }
 };
