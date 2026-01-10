61f6557bd03efbe69f275a9113202617eafa38f0
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-01-10 15:59:50 -0600

    Refactor maxArea function in Container_With_Most_Water
    Fix variable names and incomplete logic for max area calculation.

--- medium/11_Container_With_Most_Water.cpp
+++ medium/11_Container_With_Most_Water.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int maxWater   = 0;
+        int lowIndex = 0;
+        int riIndex = height[height.size() - 1];
+        while((rightIndex - leftIndex) >= 1) {
+            maxWater = height[rightIndex] - 
+        }
         
+        return maxWater;
     }
 };
