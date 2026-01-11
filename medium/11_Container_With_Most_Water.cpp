349023f97f11ac3ccb823ea023c68520e41c591b
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-01-10 22:44:11 -0600

    Simplify index increment logic in water container algorithm
    Refactor logic for determining low and high index increments.

--- medium/11_Container_With_Most_Water.cpp
+++ medium/11_Container_With_Most_Water.cpp
@@ -11,32 +11,15 @@ public:
             } else{
                 water *= height[highIndex];
             }
-            cout << height[lowIndex] << " and " << height[highIndex] << " and " << water << endl;
+            
             if(water > maxWater) {
                 maxWater = water;
             }
 
-            int lowInc  = (height[lowIndex]  < height[lowIndex +1]);
-            int highInc = (height[highIndex] < height[highIndex-1]);
-            if        ( lowInc & !highInc) {
+            else if (height[highIndex] > height[lowIndex]) {
                 lowIndex  += 1;
-            } else if (!lowInc &  highInc) {
-                highIndex -= 1;
             } else {
-                if (height[highIndex-1] == height[lowIndex+1]) {
-                    int lowIncStep  = height[lowIndex +1] - height[lowIndex];
-                    int highIncStep = height[highIndex-1] - height[highIndex];
-                    if(lowIncStep > highIncStep) {
-                        lowIndex  += 1;
-                    } else {
-                        highIndex -= 1;
-                    }
-                }
-                else if (height[highIndex-1] > height[lowIndex+1]) {
-                    lowIndex  += 1;
-                } else {
-                    highIndex -= 1;
-                }
+                highIndex -= 1;
             }
         }
         return maxWater;
