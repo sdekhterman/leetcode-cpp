eb19b6707e673f67eb6d6c21fcf8019d5ba31e4f
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-01-10 22:06:36 -0600

    Update 11_Container_With_Most_Water.cpp
    There are so many edge cases :'( .

--- medium/11_Container_With_Most_Water.cpp
+++ medium/11_Container_With_Most_Water.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int maxWater   = 0;
-        int lowIndex = 0;
-        int riIndex = height[height.size() - 1];
-        while((rightIndex - leftIndex) >= 1) {
-            maxWater = height[rightIndex] - 
+        int maxWater  = 0;
+        int lowIndex  = 0;
+        int highIndex = height.size()-1;
+        while((highIndex - lowIndex) > 0) {
+            int water = highIndex - lowIndex;
+            if(height[lowIndex] < height[highIndex]) {
+                water *= height[lowIndex];
+            } else{
+                water *= height[highIndex];
+            }
+            cout << height[lowIndex] << " and " << height[highIndex] << " and " << water << endl;
+            if(water > maxWater) {
+                maxWater = water;
+            }
+
+            int lowInc  = (height[lowIndex]  < height[lowIndex +1]);
+            int highInc = (height[highIndex] < height[highIndex-1]);
+            if        ( lowInc & !highInc) {
+                lowIndex  += 1;
+            } else if (!lowInc &  highInc) {
+                highIndex -= 1;
+            } else {
+                if (height[highIndex-1] == height[lowIndex+1]) {
+                    int lowIncStep  = height[lowIndex +1] - height[lowIndex];
+                    int highIncStep = height[highIndex-1] - height[highIndex];
+                    if(lowIncStep > highIncStep) {
+                        lowIndex  += 1;
+                    } else {
+                        highIndex -= 1;
+                    }
+                }
+                else if (height[highIndex-1] > height[lowIndex+1]) {
+                    lowIndex  += 1;
+                } else {
+                    highIndex -= 1;
+                }
+            }
         }
-        
         return maxWater;
     }
 };
