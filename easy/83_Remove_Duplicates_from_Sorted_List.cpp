4502a6c49d11165f71fa3a5e90cfc79db8a4fa44
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-01-18 17:25:08 -0600

    Add solution for removing duplicates from sorted list
    Implement initial structure for removing duplicates from a sorted linked list.

---
+++ easy/83_Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head.next != nullptr){
+            if(head.val == head.next.val) {
+                if(head.next.next != nullptr) {
+                }
+                else{
+
+                }
+            } 
+            else {
+
+            }
+        }
+        return head;
+    }
+};
