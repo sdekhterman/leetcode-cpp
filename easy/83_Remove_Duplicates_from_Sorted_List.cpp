1b5f313e3a70ef2dc185a61290b46dd935b5cb6d
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-01-18 18:43:55 -0600

    Add comments for alternative duplicate removal approach
    Commented out an alternative solution for removing duplicates from a sorted linked list.

--- easy/83_Remove_Duplicates_from_Sorted_List.cpp
+++ easy/83_Remove_Duplicates_from_Sorted_List.cpp
@@ -12,6 +12,17 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head == nullptr) {return head;}
+        
+        // best solution on leetcode moved a pointer along the linked list to modify the list (head still point to the front)
+        // ListNode* cur = head;
+        // while (cur->next != nullptr) {
+        //     if (cur->val == cur->next->val) {
+        //         cur->next = cur->next->next;
+        //     } else {
+        //         cur = cur->next;
+        //     }
+        // }
+        
         if(head->next != nullptr){
             if(head->val == head->next->val) {
                 if(head->next->next != nullptr) {
