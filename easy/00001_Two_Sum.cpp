ef243179f2cada2ec50c4698d414870763adfc97
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2026-05-25 16:52:24 -0500

    Refactor twoSum function to clear index_pair initialization
    Removed initialization of index_pair and commented out alternative logic.

--- easy/00001_Two_Sum.cpp
+++ easy/00001_Two_Sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> umap; // hash map <key type,value type>
-        std::vector<int> index_pair = {0,1};
+        std::vector<int> index_pair = {};
         int value = 0; //safe to pick since start search at 0 and we have an anti-duplicate index check
 
         // The last instance of a key is maintained
@@ -19,5 +19,15 @@ public:
             }
         }
         return index_pair;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     value = umap[target-nums[i]];
+        //     if((target == (nums[value] + nums[i])) && (i != value)){
+        //         index_pair = {i,value};
+        //     }
+        //     // placed at bottom so not stuck with repeat value index of duplicate
+        //     umap[nums[i]] = i;
+        // }
+        // return index_pair;
     }
 };
